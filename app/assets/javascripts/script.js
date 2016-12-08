4d25a44cd5190718e8c595aae32647cbfb8c19db
Traci7822 <Tracir@gmail.com>
2016-12-08 15:23:36 -0700

    refactored

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,37 +1,24 @@
 $(document).ready(function() {
+  hideSections();
+
   $(".navItem").click(function(event) {
     event.preventDefault();
-
-    $(".navItem").removeClass("active").removeClass("on");
-    $(this).addClass("active").addClass("on");
-    var panel = $(this).attr('id');
-    var items = document.querySelectorAll(".navItem");
-    for (var i = 0; i < items.length; i++) {
-      $("#" + items[i].id + "-content").hide();
-    }
-    $("#" + panel + "-content").show();
+    showPanel(this);
   })
 
 });
 
+function showPanel(section){
+  $(".navItem").removeClass("active");
+  $(section).addClass("active");
+  var panel = $(section).attr('id');
+  hideSections();
+  $("#" + panel + "-content").show();
+}
 
-// $("#about-content").hide()
-// $("#projects-content").hide()
-// $("#resume-content").hide()
-// $("#blog-content").hide()
-// $("#contact-content").hide()
-//
-// $("#about").click(function(){
-//   $("#about-content").show()
-//   $("#projects-content").hide()
-//   $("#resume-content").hide()
-//   $("#blog-content").hide()
-//   $("#contact-content").hide()
-// })
-//
-// $("#projects").click(function(){
-//   $("#about-content").hide()
-//   $("#projects-content").show()
-//   $("#resume-content").hide()
-//   $("#blog-content").hide()
-//   $("#contact-content").hide()
+function hideSections() {
+  var items = document.querySelectorAll(".navItem");
+  for (var i = 0; i < items.length; i++) {
+    $("#" + items[i].id + "-content").hide();
+  }
+}

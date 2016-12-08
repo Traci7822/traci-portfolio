cec12fe9dcbc699763aa03ea763043ddcfcf519f
Traci7822 <Tracir@gmail.com>
2016-12-08 15:26:18 -0700

    refactored

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -8,17 +8,17 @@ $(document).ready(function() {
 
 });
 
+function hideSections() {
+  var items = document.querySelectorAll(".navItem");
+  for (var i = 0; i < items.length; i++) {
+    $("#" + items[i].id + "-content").hide();
+  }
+}
+
 function showPanel(section){
   $(".navItem").removeClass("active");
   $(section).addClass("active");
   var panel = $(section).attr('id');
   hideSections();
   $("#" + panel + "-content").show();
-}
-
-function hideSections() {
-  var items = document.querySelectorAll(".navItem");
-  for (var i = 0; i < items.length; i++) {
-    $("#" + items[i].id + "-content").hide();
-  }
 }
